8d63b009fc09a17f890ae4ef4ede88c00866b8ec
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-11-29 15:38:48 +0100

    Import the new design.

--- public/config.rb
+++ public/config.rb
@@ -1,26 +1,22 @@
-
-require 'zurb-foundation'
 # Require any additional compass plugins here.
-
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = ( environment == :production ) ? :compressed : :expanded
-
-# Enable source map
-sass_options = { :debug_info => ( environment == :development ) }
+# output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
